9eea4759dc100be70973d0cfa79d70120e198d0a
alans2021 <als432@pitt.edu>
2019-11-29 19:03:26 -0500

    Prints full joint distribution of bayes net

--- Assignment3/a3.py
+++ Assignment3/a3.py
@@ -3,6 +3,7 @@ from tree import Choice
 from tree import Decision
 from probability import BayesNet
 from probability import enumeration_ask
+from probability import BayesNode
 
 
 # First create all nodes with connections and probabilities. Then find the value of first decision node
@@ -47,39 +48,80 @@ def q2():
     return 'Value for D1 is ' + str(D1.value()) + ' and ' + 'Action from D1 is ' + D1.action
 
 
-def q6(variable, conditions, value):
-    T, F = True, False
-    chatbot = BayesNet([
-        ('Accurate', '', 0.9),
-        ('ProblemSize', '', 0.9),   # 0.9 is probabililty that problem size is small
-        ('ConversationLength', 'ProblemSize', {T: (0.40, 0.40, 0.20), F: (0.20, 0.30, 0.50)}, 3),
-        ('Frustrated', 'Accurate ProblemSize ConversationLength',
-         {(T, T, 0): 0.20, (T, T, 1): 0.30, (T, T, 2): 0.60,
-          (T, F, 0): 0.30, (T, F, 1): 0.60, (T, F, 2): 0.70,
-          (F, T, 0): 0.40, (F, T, 1): 0.50, (F, T, 2): 0.80,
-          (F, F, 0): 0.50, (F, F, 1): 0.80, (F, F, 2): 0.90}),
-        ('Resolved', 'Accurate ConversationLength',
-         {(T, 0): 0.30, (T, 1): 0.50, (T, 2): 0.70,
-          (F, 0): 0.20, (F, 1): 0.30, (F, 2): 0.40})
-    ])
+T, F = True, False
+chatbot = BayesNet([
+    ('Accurate', '', 0.9),
+    ('ProblemSize', '', 0.9),   # 0.9 is probabililty that problem size is small
+    ('ConversationLength', 'ProblemSize', {T: (0.40, 0.40, 0.20), F: (0.20, 0.30, 0.50)}, 3),
+    ('Frustrated', 'Accurate ProblemSize ConversationLength',
+     {(T, T, 0): 0.20, (T, T, 1): 0.30, (T, T, 2): 0.60,
+      (T, F, 0): 0.30, (T, F, 1): 0.60, (T, F, 2): 0.70,
+      (F, T, 0): 0.40, (F, T, 1): 0.50, (F, T, 2): 0.80,
+      (F, F, 0): 0.50, (F, F, 1): 0.80, (F, F, 2): 0.90}),
+    ('Resolved', 'Accurate ConversationLength',
+     {(T, 0): 0.30, (T, 1): 0.50, (T, 2): 0.70,
+      (F, 0): 0.20, (F, 1): 0.30, (F, 2): 0.40})
+])
+psize_dict = {True: 'Small', False: 'Big'}
+clength_dict = {0: 'Short', 1: 'Medium', 2: 'Long'}
 
+
+def q8(variable, conditions, value):
+
+    # Do enumeration ask
     probdist = enumeration_ask(variable, conditions, chatbot)
 
-    psize_dict = {True: 'Small', False: 'Big'}
-    clength_dict = {0: 'Short', 1: 'Medium', 2: 'Long'}
+    # Parse input to output in correct format
+
     for var in conditions:
         if var == 'ProblemSize':
             conditions[var] = psize_dict[conditions[var]]
         if var == 'ConversationLength':
             conditions[var] = clength_dict[conditions[var]]
+    prob = round(probdist.prob[value], 4)
+
+    if type(value) == int:
+        value = clength_dict[value]
 
     return 'Probability of ' + variable + ' being ' + str(value) + ' given ' + \
-           str(conditions) + ': ' + str(probdist.prob[value])
+           str(conditions) + ': ' + str(prob)
+
+
+def q9():
+    # Full joint distribution of bayes net
+    print('Full Joint Distribution of Bayes Net:')
+    accurate = [True, False]
+    psize = [True, False]
+    clength = [0, 1, 2]
+    resolved = [True, False]
+    frustrated = [True, False]
+
+    nodea = chatbot.variable_node('Accurate')
+    nodep = chatbot.variable_node('ProblemSize')
+    nodec = chatbot.variable_node('ConversationLength')
+    noder = chatbot.variable_node('Resolved')
+    nodef = chatbot.variable_node('Frustrated')
+
+    for a in accurate:
+        proba = nodea.p(a, {})
+        for p in psize:
+            probp = nodep.p(p, {})
+            for c in clength:
+                probc = nodec.p(c, {'ProblemSize': p})
+                for r in resolved:
+                    probr = noder.p(r, {'ConversationLength': c, 'Accurate': a})
+                    for f in frustrated:
+                        probf = nodef.p(f, {'ProblemSize': p, 'ConversationLength': c, 'Accurate': a})
+                        finalprob = round(proba * probp * probc * probr * probf, 6)
+                        print('Probability when Accuracy = ' + str(a) + ', Problem_Size = ' + psize_dict[p] +
+                              ', Conversation_Length = ' + clength_dict[c] + ', Resolved = ' + str(r) +
+                              ', Frustrated = ' + str(f) + ': ' + str(finalprob))
 
 
 if __name__ == '__main__':
-    answer = q2()
-    print(answer)
-
-    a = q6('ConversationLength', dict(ProblemSize=True), 0)
+    # answer = q2()
+    # print(answer)
+    #
+    a = q8('Resolved', dict(ProblemSize=True), True)
     print(a)
+    q9()
